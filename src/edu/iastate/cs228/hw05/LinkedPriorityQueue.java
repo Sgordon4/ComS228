4b6fc8546781c504766bd227ae01e792a99c7315
Sean Gordon <SeanGordonkh@gmail.com>
2018-10-05 15:40:47 -0500

    LinkedPriorityQueue half completed, decided to overhaul add method to keep highest priority at start, this version is simply a backup.

--- src/edu/iastate/cs228/hw05/LinkedPriorityQueue.java
+++ src/edu/iastate/cs228/hw05/LinkedPriorityQueue.java
@@ -4,6 +4,7 @@ package edu.iastate.cs228.hw05;
  * A class of priority queues represented by a chain of linked nodes.
  * 
  * @author
+ * Sean Gordon
  * 
  * NOTE: 
  * 0. Put your Firstname and Lastname after above author tag.
@@ -30,118 +31,145 @@ package edu.iastate.cs228.hw05;
 
 
 public final class LinkedPriorityQueue<T extends Comparable<? super T>>
-                                         implements PriorityQueueInterface<T>
+implements PriorityQueueInterface<T>
 {
-   private Node firstNode; // Reference to first node of chain and the front
-                           // of the priority queue, which has the highest priority
-   private int  length;    // Number of entries in chain
+	private Node firstNode; // Reference to first node of chain and the front
+	// of the priority queue, which has the highest priority
+	private int  length;    // Number of entries in chain
 
-   public LinkedPriorityQueue()
-   {
-      firstNode = null;
-      length = 0;
-   }
+	public LinkedPriorityQueue()
+	{
+		firstNode = null;
+		length = 0;
+	}
 
-   public void add(T newEntry)
-   {
-   	//TODO
-   	return;
-   } 
+	public void add(T newEntry)
+	{
+		if(newEntry == null)
+			throw new IllegalArgumentException();
 
-   public T remove()
-   {
-    //TODO
-   	return null;
-   }
+		Node newNode = new Node(newEntry);
+		
+		if(length == 0) 
+			firstNode = newNode;
+		
+		else {
+			Node temp = firstNode;
+			while(temp.getNextNode() != null && 
+				  temp.getNextNode().getData().compareTo(newEntry) < 0) {
+				temp = temp.getNextNode();
+			}
+			
+			newNode.setNextNode(temp.getNextNode());
+			temp.setNextNode(newNode);
+		}
+		
+		length++;
+	} 
 
-   public T peek()
-   {
-    //TODO
-   	return null;
-   }
+	public T remove()
+	{
+		if(length == 0)
+			return null;
+		
+		Node temp = firstNode;
+		
+		while(temp.getNextNode() != null) {
+			//Decided here to change firstNode to node with highest
+			//priority to more efficiently work through list 
+		}
+		
+		return null;
+	}
 
-   /**
-    * If queue is empty returns [].
-    * Else, returns as [1, 2, 3]
-    * Important: note a comma and single space before every
-    * item except the last, and after last there is no space.
-    * In both cases before and after square brackets there
-    * is no space.
-    *    
-    */
-   @Override
-   public String toString()
-   {
-   	//TODO
-   	return null;
-   }
-   
-   
-   public boolean isEmpty()
-   {
-      boolean result;
-            
-      if (length == 0)
-      {
-         assert firstNode == null;
-         result = true;
-      }
-      else
-      {
-         assert firstNode != null;
-         result = false;
-      }
-         
-      return result;
-   }
+	public T peek()
+	{
+		//TODO
+		return null;
+	}
 
-   public int getSize()
-   {
-      return length;
-   }
+	/**
+	 * If queue is empty returns [].
+	 * Else, returns as [1, 2, 3]
+	 * Important: note a comma and single space before every
+	 * item except the last, and after last there is no space.
+	 * In both cases before and after square brackets there
+	 * is no space.
+	 *    
+	 */
+	@Override
+	public String toString()
+	{
+		//TODO
+		return null;
+	}
 
-   public void clear()
-   {
-      firstNode = null;
-      length = 0;
-   }
-   
-   
-   private class Node
-   {
-      private T    data; // Entry in priority queue
-      private Node next; // Link to next node
 
-      private Node(T dataPortion) 
-      {
-         data = dataPortion;
-         next = null;	
-      }
-      
-      private Node(T dataPortion, Node nextNode) 
-      {
-         data = dataPortion;
-         next = nextNode;	
-      }
-      
-      private T getData()
-      {
-         return data;
-      }
-      
-      private void setData(T newData)
-      {
-         data = newData;
-      }
-      
-      private Node getNextNode()
-      {
-         return next;
-      }
-      
-      private void setNextNode(Node nextNode)
-      {
-         next = nextNode;
-      }
-   }
+	public boolean isEmpty()
+	{
+		boolean result;
+
+		if (length == 0)
+		{
+			assert firstNode == null;
+			result = true;
+		}
+		else
+		{
+			assert firstNode != null;
+			result = false;
+		}
+
+		return result;
+	}
+
+	public int getSize()
+	{
+		return length;
+	}
+
+	public void clear()
+	{
+		firstNode = null;
+		length = 0;
+	}
+
+
+	private class Node
+	{
+		private T    data; // Entry in priority queue
+		private Node next; // Link to next node
+
+		private Node(T dataPortion) 
+		{
+			data = dataPortion;
+			next = null;	
+		}
+
+		private Node(T dataPortion, Node nextNode) 
+		{
+			data = dataPortion;
+			next = nextNode;	
+		}
+
+		private T getData()
+		{
+			return data;
+		}
+
+		private void setData(T newData)
+		{
+			data = newData;
+		}
+
+		private Node getNextNode()
+		{
+			return next;
+		}
+
+		private void setNextNode(Node nextNode)
+		{
+			next = nextNode;
+		}
+	}
 } // end LinkedPriorityQueue
